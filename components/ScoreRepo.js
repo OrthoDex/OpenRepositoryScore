cebd01fd2729b5b2345c52d7104d590830e23a67
Ishaan Malhi <ishaan.malhi@gmail.com>
2019-09-28 03:45:30 -0700

    feat: fix tensorflow and add sentiment analysis model

--- components/ScoreRepo.js
+++ components/ScoreRepo.js
@@ -1,52 +1,71 @@
 import React from "react";
 import { Mutation } from "react-apollo";
 import { repoQuery, addRepoScore } from "../lib/graphQueries";
-import { getSentimentAnalysis } from "../lib/predict";
+import { getSentimentAnalysis, setupModel } from "../lib/predict";
 
 export default class RepoInput extends React.Component {
   constructor() {
     super();
     this.state = {
-      textboxValue: ""
+      textboxValue: "",
+      model: null,
+      predictor: null,
+      enableText: false
     };
   }
-  
+
+  async componentDidMount() {
+    const { model, predictor } = await setupModel();
+    this.setState({
+      model,
+      predictor,
+      enableText: true
+    });
+  }
+
   handleTextboxValueChange = e => {
     this.setState({
       textboxValue: e.target.value
     });
   };
 
-  handleTextboxKeyPress = async (e, addRepo) => {
+  getSentimentAndUpdate = async (addRepo, repoUrl) => {
+    const sentiment_output = await getSentimentAnalysis(
+      repoUrl,
+      this.state.model,
+      this.state.predictor
+    );
+    console.debug(sentiment_output);
+    addRepo({
+      variables: {
+        objects: [
+          Object.assign(
+            {
+              url: repoUrl
+            },
+            sentiment_output
+          )
+        ]
+      },
+      update: (store, { data: { insert_repo } }) => {
+        const data = store.readQuery({ query: repoQuery });
+        const insertedRepo = insert_repo.returning;
+        data.repo.splice(0, 0, insertedRepo[0]);
+        store.writeQuery({
+          query: repoQuery,
+          data
+        });
+        this.setState({
+          textboxValue: ""
+        });
+      }
+    });
+  };
+
+  handleTextboxKeyPress = (e, addRepo) => {
     if (e.key === "Enter") {
       const repoUrl = this.state.textboxValue;
-      const results = await getSentimentAnalysis(repoUrl)
-      console.log(results)
-      const [...sentiment_output] = results;
-      addRepo({
-        variables: {
-          objects: [
-            Object.assign(
-              {
-                url: repoUrl
-              },
-              sentiment_output
-            )
-          ]
-        },
-        update: (store, { data: { insert_repo } }) => {
-          const data = store.readQuery({ query: repoQuery });
-          const insertedRepo = insert_repo.returning;
-          data.repo.splice(0, 0, insertedRepo[0]);
-          store.writeQuery({
-            query: repoQuery,
-            data
-          });
-          this.setState({
-            textboxValue: ""
-          });
-        }
-      });
+      this.getSentimentAndUpdate(addRepo, repoUrl);
     }
   };
 
@@ -57,6 +76,7 @@ export default class RepoInput extends React.Component {
           return (
             <div className="parentContainer">
               <input
+                disabled={!this.state.enableText}
                 className="input"
                 placeholder="Add a repo"
                 value={this.state.textboxValue}
