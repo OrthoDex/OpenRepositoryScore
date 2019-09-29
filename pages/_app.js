9aa1f972d410e8071d301fbab23da07dd4868497
Ishaan Malhi <ishaan.malhi@gmail.com>
2019-09-29 14:02:01 -0700

    feat: add bugsnag

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,21 @@ import App from "next/app";
 import React from "react";
 import withApolloClient from "../lib/with-apollo-client";
 import { ApolloProvider } from "react-apollo";
-
+import bugsnag from "@bugsnag/js";
+import bugsnagReact from "@bugsnag/plugin-react";
+const bugsnagClient = bugsnag("60aef36435f3a3fff4389ed7cf41c1c4");
+bugsnagClient.use(bugsnagReact, React);
+global.bugsnagClient = bugsnagClient;
+const ErrorBoundary = bugsnagClient.getPlugin("react");
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <ErrorBoundary>
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ErrorBoundary>
     );
   }
 }
